9e6d96884c95a520a98f4178dadf164d5852374e
Ayah Shehada <ayahshehada98@gmail.com>
2024-12-18 10:26:19 +0100

    Changed errors

--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -9,16 +9,19 @@ float Sensor::calculateAverage()
     return (std::accumulate(dataList.begin(), dataList.end(), 0.0) / dataList.size());
 }
 
-float Sensor::calculateMinimum(const std::vector<float> &dataList)
+float Sensor::calculateMinimum()
 {
+    return 0.0; // <-- To remove warnings
 }
 
-float Sensor::calculateMaximum(const std::vector<float> &dataList)
+float Sensor::calculateMaximum()
 {
+    return 0.0; // <-- To remove warnings
 }
 
 float Sensor::getLatestData()
 {
+    return 0.0; // <-- To remove warnings
 }
 
 float Sensor::getAverageData()
@@ -28,8 +31,10 @@ float Sensor::getAverageData()
 
 float Sensor::getMinimumData()
 {
+    return calculateMinimum();
 }
 
 float Sensor::getMaxData()
 {
+    return calculateMaximum();
 }
